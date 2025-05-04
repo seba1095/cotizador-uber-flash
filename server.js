65b2a330a25b0f562771cf78bad6fe654e89de1b
Sebastián Andrés González Araya <seba@MacBook-Pro-de-Sebastian.local>
2025-05-03 22:14:07 -0400

    una descripción del cambio

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const getDistanceInKm = async (origen, destino) => {
 
 const calcularCostoFlash = (km) => {
   const base = 3000;
-  const porKm = 740;
+  const porKm = 400;
   return Math.round(base + (km * porKm));
 };
 
