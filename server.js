50f4f2607b8129ebb0bf23fae7238a7be2b0c886
Sebastián Andrés González Araya <seba@MacBook-Pro-de-Sebastian.local>
2025-05-04 02:43:24 -0400

    una descripción del cambio

--- server.js
+++ server.js
@@ -51,7 +51,8 @@ app.post('/cotizar', async (req, res) => {
   try {
     const destino = req.body.request.to.address + ' ' + req.body.request.to.street_number + ', ' + req.body.request.to.municipality_name + ', ' + req.body.request.to.region_name + ', ' + 'Chile';
     
-    const km = 0;
+    const km;
+    km= 0;
     km = await getDistanceInKm(ORIGEN, destino);
     const costo = calcularCostoFlash(km)
 
